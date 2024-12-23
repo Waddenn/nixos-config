ff62c0ece5241e45f3fdb8a7de29e296ee9866a8
waddenn <waddenn.github@gmail.com>
2024-12-23 20:58:06 +0100

    renamed

---
+++ modules/common/base.nix
@@ -0,0 +1,62 @@
+{ config, pkgs, ... }:
+{
+  ###############################
+  # Boot loader, EFI, etc.
+  ###############################
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+    systemd-boot.configurationLimit = 20;
+  };
+
+  ###############################
+  # Firewall
+  ###############################
+  networking.firewall.enable = true;
+
+  ###############################
+  # Options Nix
+  ###############################
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
+
+  ###############################
+  # Garbage collection
+  ###############################
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    persistent = true;
+    options = "--delete-older-than 14d";
+  };
+
+  ###############################
+  # Mises à jour automatiques
+  ###############################
+  system.autoUpgrade = {
+    enable = true;
+    dates = "daily";
+    persistent = true;
+    flags = [ "--update-input" "nixpkgs" ];
+  };
+
+  zramSwap.enable = true;
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Paris";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fr_FR.UTF-8";
+    LC_IDENTIFICATION = "fr_FR.UTF-8";
+    LC_MEASUREMENT = "fr_FR.UTF-8";
+    LC_MONETARY = "fr_FR.UTF-8";
+    LC_NAME = "fr_FR.UTF-8";
+    LC_NUMERIC = "fr_FR.UTF-8";
+    LC_PAPER = "fr_FR.UTF-8";
+    LC_TELEPHONE = "fr_FR.UTF-8";
+    LC_TIME = "fr_FR.UTF-8";
+  };
+
+  console.keyMap = "fr";
+}
