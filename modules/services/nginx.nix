b7b276ae47783283f6b17a6530afb6d9295d0e10
waddenn <waddenn.github@gmail.com>
2024-12-23 20:40:09 +0100

    services modified

--- modules/services/nginx.nix
+++ modules/services/nginx.nix
@@ -2,16 +2,16 @@
 
 {
 
-services.nginx.enable = true;
-services.nginx.virtualHosts."myhost.org" = {
-    addSSL = true;
-    enableACME = true;
-    root = "/var/www/myhost.org";
-};
-security.acme = {
-  acceptTerms = true;
-  defaults.email = "foo@bar.com";
-};
+  services.nginx.enable = true;
+  services.nginx.virtualHosts."myhost.org" = {
+      addSSL = true;
+      enableACME = true;
+      root = "/var/www/myhost.org";
+  };
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "foo@bar.com";
+  };
 
 }
 
