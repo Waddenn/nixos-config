5474e227a0fb38c365fa53d0ca7a4e8291f198f4
Waddenn <tpatelas@proton.me>
2024-12-27 22:41:12 +0100

    modules reorganised

---
+++ modules/common/console/keyMap.nix
@@ -0,0 +1,7 @@
+{ config, pkgs, ... }:
+
+{
+
+  console.keyMap = "fr";
+  
+}
--- modules/templates/laptop.nix
+++ modules/templates/laptop.nix
@@ -4,17 +4,23 @@
 
   imports = [
 
-    ../common/steam.nix
-    ../common/firefox.nix
-    ../common/base.nix
-    ../common/direnv.nix
-    ../common/systemd-boot.nix
-    ../common/zramswap.nix
-    ../common/direnv.nix
-    ../common/firewall.nix
-    ../common/garbage-collection.nix
-    ../common/locale.nix
-    ../common/system-autoupgrade.nix
+    ../common/boot-loader/systemd-boot.nix
+    ../common/console/keyMap.nix
+    ../common/i18n/i18n.nix
+    ../common/networking/firewall.nix
+    ../common/networking/networkmanager.nix
+    ../common/nix/gc.nix
+    ../common/nix/settings.nix
+    ../common/nixpkgs/config.nix
+    ../common/programs/direnv.nix
+    ../common/programs/firefox.nix
+    ../common/programs/steam.nix
+    ../common/security/rtkit.nix
+    ../common/system/autoUpgrade.nix
+    ../common/time/timeZone.nix
+    ../common/zramSwap/zramswap.nix
+
+    ../common/hardware/bluetooth.nix
 
     ../desktop/gnome/base.nix
 
