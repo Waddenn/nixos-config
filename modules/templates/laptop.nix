ee93acd84a1a52069db8b53f228c9c07d5efacd7
waddenn <waddenn.github@gmail.com>
2024-12-28 00:02:35 +0100

    modules moved

--- modules/templates/laptop.nix
+++ modules/templates/laptop.nix
@@ -4,21 +4,21 @@
 
   imports = [
 
-    ../common/boot-loader/systemd-boot.nix
-    ../common/console/keyMap.nix
-    ../common/i18n/i18n.nix
-    ../common/networking/firewall.nix
-    ../common/networking/networkmanager.nix
-    ../common/nix/gc.nix
-    ../common/nix/settings.nix
-    ../common/nixpkgs/config.nix
-    ../common/programs/direnv.nix
-    ../common/programs/firefox.nix
-    ../common/programs/steam.nix
-    ../common/security/rtkit.nix
-    ../common/system/autoUpgrade.nix
-    ../common/time/timeZone.nix
-    ../common/zramSwap/zramswap.nix
+    ../boot-loader/systemd-boot.nix
+    ../console/keyMap.nix
+    ../i18n/i18n.nix
+    ../networking/firewall.nix
+    ../networking/networkmanager.nix
+    ../nix/gc.nix
+    ../nix/settings.nix
+    ../nixpkgs/config.nix
+    ../programs/direnv.nix
+    ../programs/firefox.nix
+    ../programs/steam.nix
+    ../security/rtkit.nix
+    ../system/autoUpgrade.nix
+    ../time/timeZone.nix
+    ../zramSwap/zramswap.nix
 
     ../hardware/bluetooth.nix
 
