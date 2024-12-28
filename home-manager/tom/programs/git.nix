2462f81542abaaa974ec0cdb036b54fc4b093e37
waddenn <waddenn.github@gmail.com>
2024-12-28 21:40:46 +0100

    home manager refactored

---
+++ home-manager/tom/programs/git.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+
+{
+    programs.git = {
+    enable = true;
+    userName  = "waddenn";
+    userEmail = "waddenn.github@gmail.com";
+    extraConfig = {
+      init.defaultBranch = "main";
+    };
+  };
+
+}
