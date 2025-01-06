77883e79e65e454761ab8099bf58b4fff8906df3
waddenn <waddenn.github@gmail.com>
2025-01-06 10:22:02 +0100

    fix autoupgrade

--- modules/system/autoUpgrade.nix
+++ modules/system/autoUpgrade.nix
@@ -1,12 +1,17 @@
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
 
   system.autoUpgrade = {
     enable = true;
     dates = "daily";
+    flake = inputs.self.outPath;
+    flags = [
+      "--update-input"
+      "nixpkgs"
+      "-L" 
+    ];
     persistent = true;
-    flags = [ "--update-input" "nixpkgs" ];
   };
 
 }
