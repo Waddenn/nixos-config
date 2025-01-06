a3491d8d5c7ca8898ac3f771c5696aa2842c29fb
Waddenn <tpatelas@proton.me>
2025-01-06 16:23:19 +0100

    fix grub

--- modules/boot-loader/grub.nix
+++ modules/boot-loader/grub.nix
@@ -2,7 +2,5 @@
 
 {
   boot.loader.grub.enable = true;
-  boot.loader.grub.devices = [ "nodev" ];
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.useOSProber = true;
+  boot.loader.grub.devices = [ "dev/sda" ];
 }
