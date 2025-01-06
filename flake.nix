654b6f942e4a627e3caa654d1204751d6060eb30
waddenn <waddenn.github@gmail.com>
2025-01-06 15:46:07 +0100

    vm test

--- flake.nix
+++ flake.nix
@@ -38,25 +38,24 @@
         ];
       };
 
-      # nixos-vm-nextcloud = let
-      #   username = "tom";
-      #   hostname = "nixos-vm-nextcloud";
-      #   specialArgs = { inherit inputs username; };
-      # in nixpkgs.lib.nixosSystem {
-      #   system = "x86_64-linux";
-      #   specialArgs = specialArgs;
-      #   modules = 
-      #   [
-      #     ./hosts/${hostname}/shared/configuration.nix
-      #     ./hosts/${hostname}/users/tom.nix
-      #     home-manager.nixosModules.home-manager
-      #     {
-      #       home-manager.useGlobalPkgs = true;
-      #       home-manager.useUserPackages = true;
-      #       home-manager.users.${username} = import ./home-manager/users/tom.nix;
-      #     }
-      #   ];
-      # };
+      vm-test = let
+        username = "tom";
+        hostname = "vm-test";
+        specialArgs = { inherit inputs username; };
+      in nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = specialArgs;
+        modules = 
+        [
+          ./hosts/${hostname}/hardware-configuration.nix
+          ./users/${username}/default.nix
+          ./modules/templates/server.nix
+          {
+            networking.hostName = hostname;
+            system.stateVersion = "25.05";
+          }
+        ];
+      };
     };
   };
 }
