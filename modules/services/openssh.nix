f4839c00cd4cfbad37c74e4be2afa1aa413aea2a
Waddenn <tpatelas@proton.me>
2025-01-07 11:09:19 +0100

    update openssh

--- modules/services/openssh.nix
+++ modules/services/openssh.nix
@@ -4,13 +4,8 @@
 
 services.openssh = {
   enable = true;
-  ports = [ 22 ];
   settings = {
-    PasswordAuthentication = true;
-    AllowUsers = null; 
-    UseDns = true;
-    X11Forwarding = false;
-    PermitRootLogin = "prohibit-password"; 
+    PermitRootLogin = "yes"; 
   };
 };
 
