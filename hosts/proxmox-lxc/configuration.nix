c7eccc59c0e0e1225f56bcb35e5842fae9dfd676
waddenn <waddenn.github@gmail.com>
2025-01-07 11:52:36 +0100

    configuration.nix added

---
+++ hosts/proxmox-lxc/configuration.nix
@@ -0,0 +1,23 @@
+{ config, modulesPath, pkgs, lib, ... }:
+
+{
+
+  imports = [ (modulesPath + "/virtualisation/proxmox-lxc.nix") ];
+  nix.settings = { sandbox = false; };  
+  proxmoxLXC = {
+    manageNetwork = false;
+    privileged = true;
+  };
+  
+  security.pam.services.sshd.allowNullPassword = true;
+  services.openssh = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+        PermitRootLogin = "yes";
+        PasswordAuthentication = true;
+    };
+  };
+  system.stateVersion = "24.11";
+  
+}
