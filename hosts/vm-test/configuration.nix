cd0521b92232aedf6945d11754fac61f05264010
Waddenn <tpatelas@proton.me>
2025-01-07 11:37:29 +0100

    first commit

--- hosts/vm-test/configuration.nix
+++ hosts/vm-test/configuration.nix
@@ -3,9 +3,23 @@
 
 {
 
-  environment.systemPackages =  with pkgs; [
-    git
+  imports = [
+    ../zramSwap/zramswap.nix
+    ../services/tailscale.nix
+    ../console/keyMap.nix
+    ../i18n/i18n.nix
+    ../networking/networkmanager.nix
+    ../nix/settings.nix
+    ../nixpkgs/config.nix
+    ../time/timeZone.nix
+    ../services/openssh.nix
+    ../services/docker.nix
+    ../boot/loader/grub.nix
   ];
 
+  environment.systemPackages =  with pkgs; [
+    git
+    
+  ];
 
 }
