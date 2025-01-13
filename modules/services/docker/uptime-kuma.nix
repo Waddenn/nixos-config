ad8d20c2514a8cd17c51a5bf964f6a3dffadbd34
waddenn <waddenn.github@gmail.com>
2025-01-13 13:14:22 +0100

    feat: backend docker

--- modules/services/docker/uptime-kuma.nix
+++ modules/services/docker/uptime-kuma.nix
@@ -1,12 +1,16 @@
 { config, pkgs, ... }:
 
 {
-  virtualisation.oci-containers.containers.uptime-kuma = {
-    image = "louislam/uptime-kuma:latest";
-    ports = [ "3001:3001" ];
-    volumes = [ "/data/uptime-kuma:/app/data" ];
+  virtualisation.oci-containers = {
+    backend = "docker"; 
+    containers = {
+      uptime-kuma = {
+        image = "louislam/uptime-kuma:latest"; 
+        ports = [ "3001:3001" ];
+        volumes = [ "/data/uptime-kuma:/app/data" ]; 
+      };
+    };
   };
 
-  networking.firewall.allowedTCPPorts = [ 3001 ];
-
+  networking.firewall.allowedTCPPorts = [ 3001 ]; 
 }
