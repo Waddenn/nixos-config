c35728ec69f4f8b97c1d28713a0c2f8c80be6405
Waddenn <tpatelas@proton.me>
2025-01-13 09:43:14 +0100

    first commit

---
+++ modules/services/docker/uptime-kuma.nix
@@ -0,0 +1,10 @@
+{ config, pkgs, ... }:
+
+{
+  virtualisation.oci-containers.containers.uptime-kuma = {
+    image = "louislam/uptime-kuma:latest";
+    ports = [ "3001:3001" ];
+    volumes = [ "/data/uptime-kuma:/app/data" ];
+    autoUpgrade = true;
+  };
+}
