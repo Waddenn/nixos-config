ed77759c849d50eb18b95d342f4d959f502a4dad
waddenn <waddenn.github@gmail.com>
2025-01-17 18:26:32 +0100

    feat: kernel option

--- modules/boot/kernel.nix
+++ modules/boot/kernel.nix
@@ -1,7 +1,10 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
+  options.linuxPackages_latest.enable = lib.mkEnableOption "linuxPackages_latest";
 
-  boot.kernelPackages = pkgs.linuxPackages_latest; 
+  config = lib.mkIf config.linuxPackages_latest.enable {
+    boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_11;
 
+  };
 }
