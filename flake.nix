3ec2c2ec4d3b567f650cb4cdbeec38fd988fd529
waddenn <waddenn.github@gmail.com>
2025-01-18 15:11:18 +0100

    feat: mkSystem

--- flake.nix
+++ flake.nix
@@ -3,153 +3,57 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nix-flatpak.url = "github:gmodena/nix-flatpak";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-flatpak.url = "github:gmodena/nix-flatpak";
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, nix-flatpak, ... }:
-  {
-    nixosConfigurations = {
-      asus-nixos = let
-        username = "tom";
-        hostname = "asus-nixos";
-        specialArgs = { inherit inputs username; };
-      in nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = specialArgs;
-        modules = 
-        [
-          ./hosts/${hostname}/hardware-configuration.nix
-          ./hosts/${hostname}/configuration.nix
-          ./users/${username}/default.nix
-          home-manager.nixosModules.home-manager
-          nix-flatpak.nixosModules.nix-flatpak
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.${username} = import ./home-manager/${username}/home.nix;
+  outputs = { self, nixpkgs, home-manager, nix-flatpak, ... }:
+    let
+      lib = nixpkgs.lib;
+      mkSystems = import ./lib/mkSystems.nix { inherit nixpkgs home-manager nix-flatpak; };
+    in
+    {
+      nixosConfigurations = {
+        asus-nixos =
+          lib.nixosSystem (mkSystems.mkDesktopSystem {
+            hostname = "asus-nixos";
+            username = "tom";
+          });
 
-            networking.hostName = hostname;
-            system.stateVersion = "25.05";
-          }
-        ];
+        lenovo-nixos =
+          lib.nixosSystem (mkSystems.mkDesktopSystem {
+            hostname = "lenovo-nixos";
+            username = "tom";
+          });
+
+        tailscale-subnet =
+          lib.nixosSystem (mkSystems.mkServerSystem {
+            modules = [
+              ./modules/global.nix
+              ./modules/templates/proxmox-lxc.nix
+              { tailscale-server.enable = true; }
+            ];
+          });
+                uptime-kuma = lib.nixosSystem (mkSystems.mkServerSystem {
+          modules = [
+            ./modules/templates/proxmox-lxc.nix
+            ./modules/services/docker/uptime-kuma.nix
+          ];
+        });
+
+        searxng = lib.nixosSystem (mkSystems.mkServerSystem {
+          modules = [
+            ./modules/global.nix
+            ./modules/templates/proxmox-lxc.nix
+            {
+              tailscale-server.enable = true;
+              docker.enable = true;
+            }
+          ];
+        });
       };
-
-      lenovo-nixos = let
-        username = "tom";
-        hostname = "lenovo-nixos";
-        specialArgs = { inherit inputs username; };
-      in nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = specialArgs;
-        modules = 
-        [
-          ./hosts/${hostname}/hardware-configuration.nix
-          ./hosts/${hostname}/configuration.nix
-          ./users/${username}/default.nix
-          home-manager.nixosModules.home-manager
-          nix-flatpak.nixosModules.nix-flatpak
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.${username} = import ./home-manager/${username}/home.nix;
-
-            networking.hostName = hostname;
-            system.stateVersion = "25.05";
-          }
-        ];
-      };
-
-      tailscale-subnet = let
-        username = "nixos";
-        specialArgs = { inherit inputs username; };
-      in nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = specialArgs;
-        modules = 
-        [
-          ./modules/global.nix
-          ./modules/templates/proxmox-lxc.nix
-          ./users/${username}/default.nix
-          {
-            system.stateVersion = "25.05";
-            tailscale-server.enable = true;
-          }
-        ];
-      };
-
-      uptime-kuma = let
-        username = "nixos";
-        specialArgs = { inherit inputs username; };
-      in nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = specialArgs;
-        modules = 
-        [
-          ./modules/templates/proxmox-lxc.nix
-          ./modules/services/docker/uptime-kuma.nix
-          ./users/${username}/default.nix
-          {
-            system.stateVersion = "25.05";
-          }
-        ];
-      };
-
-      adguardhome = let
-        username = "nixos";
-        specialArgs = { inherit inputs username; };
-      in nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = specialArgs;
-        modules = 
-        [
-          ./modules/templates/proxmox-lxc.nix
-          ./modules/services/docker/adguardhome.nix
-          ./users/${username}/default.nix
-          {
-            system.stateVersion = "25.05";
-          }
-        ];
-      };
-
-      searxng = let
-        username = "nixos";
-        specialArgs = { inherit inputs username; };
-      in nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = specialArgs;
-        modules = 
-        [
-          ./modules/global.nix
-          ./modules/templates/proxmox-lxc.nix
-          ./users/${username}/default.nix
-          {
-            system.stateVersion = "25.05";
-            tailscale-server.enable = true;
-            docker.enable = true;
-          }
-        ];
-      };
-
-      ansible = let
-        username = "nixos";
-        specialArgs = { inherit inputs username; };
-      in nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = specialArgs;
-        modules = 
-        [
-          ./modules/templates/proxmox-lxc.nix
-          ./users/${username}/default.nix
-          {
-            system.stateVersion = "25.05";
-          }
-        ];
-      };
-      
-    };
-  };
+    }
 }
---
+++ lib/mkSystems.nix
@@ -0,0 +1,38 @@
+{ nixpkgs, home-manager, nix-flatpak }:
+let
+  lib = nixpkgs.lib;
+in
+{
+  mkDesktopSystem = { hostname, username, ... }:
+    {
+      system = "x86_64-linux";
+      specialArgs = { inherit nixpkgs home-manager nix-flatpak username; };
+      modules = [
+        ./hosts/${hostname}/hardware-configuration.nix
+        ./hosts/${hostname}/configuration.nix
+        ./users/${username}/default.nix
+        home-manager.nixosModules.home-manager
+        nix-flatpak.nixosModules.nix-flatpak
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.${username} = import ./home-manager/${username}/home.nix;
+          networking.hostName = hostname;
+          system.stateVersion = "25.05";
+        }
+      ];
+    };
+
+  mkServerSystem = { modules, extraConfig ? {} }:
+    {
+      system = "x86_64-linux";
+      specialArgs = { inherit nixpkgs home-manager nix-flatpak; username = "nixos"; };
+      modules = modules ++ [
+        ./users/nixos/default.nix
+        {
+          system.stateVersion = "25.05";
+        }
+        extraConfig
+      ];
+    };
+}
