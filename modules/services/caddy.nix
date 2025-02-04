46e6697400a702c07c4e806bc8d54c4fd6ddd9d2
waddenn <waddenn.github@gmail.com>
2025-02-04 11:32:03 +0100

    feat: update Caddy virtual host configuration for Calibre service

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -7,7 +7,7 @@
 
   services.caddy = {
     enable = true;
-    virtualHosts."hexaflare.net".extraConfig = ''
+    virtualHosts."calibre.hexaflare.net".extraConfig = ''
       reverse_proxy http://192.168.1.122:8080
     '';
   };
