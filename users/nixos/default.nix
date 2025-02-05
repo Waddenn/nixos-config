d492b5f178ce549230f434c4d3de4020286db940
waddenn <waddenn.github@gmail.com>
2025-02-05 15:47:22 +0100

    feat: add authorized SSH key for Ansible user in NixOS configuration

--- users/nixos/default.nix
+++ users/nixos/default.nix
@@ -8,6 +8,7 @@
       extraGroups = [ "wheel" "docker" ];
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILDWrexcT0dL92oAYuMxLpS+2WxBzwYA38C/paRGsZ2i tom@asus-nixos"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICD1VTcZIdDjDlVRftcMK20aEaK9bgHi1KA4MSLqGlsA nixos@ansible"
       ];
     };
     
