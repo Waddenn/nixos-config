9cd6eeb1732c87dd12beacf0dd2ef468d93b89fb
Waddenn <tpatelas@proton.me>
2025-02-09 15:54:59 +0100

    feat: refactor caddy service configuration to streamline virtual host settings and remove commented code

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -9,37 +9,13 @@
     enable = true;  
     logDir = "/var/log/caddy";  
     dataDir = "/var/lib/caddy"; 
-    extraConfig = ''
-      hexaflare.net {
-        reverse_proxy 192.168.1.110:8081
-        tls {
-          dns cloudflare {env.CF_API_TOKEN}  
-        }
+    virtualHosts."hexaflare.net".extraConfig = ''
+    reverse_proxy http://192.168.1.110:8081
+    tls {
+        dns cloudflare {env.CF_API_TOKEN}  
       }
-    '';
+  '';
   };
-
-  # environment.variables = {
-  #   CF_API_TOKEN = "ton_api_token";  
-  # };
-
-  systemd.services.caddy = {
-    after = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
-  };
-
-  services.caddy.package = pkgs.caddy.overrideAttrs (old: {
-    buildInputs = (old.buildInputs or []) ++ [ pkgs.go ];
-    nativeBuildInputs = (old.nativeBuildInputs or []) ++ [ pkgs.xcaddy ];
-    postInstall = (old.postInstall or "") + ''
-      export GOPROXY=https://proxy.golang.org,direct
-      export GOSUMDB=off
-      export GOFLAGS="-mod=mod"  
-      xcaddy build --with github.com/caddy-dns/cloudflare
-    '';
-  });
-
-
 
   };
 }
