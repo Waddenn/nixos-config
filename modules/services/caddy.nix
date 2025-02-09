1ec8fa1c8ec76f551036cd3ae0c9757e2345933a
Waddenn <tpatelas@proton.me>
2025-02-09 17:52:38 +0100

    feat: update Caddy service configuration for Valheim virtual host and adjust allowed TCP ports

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -1,34 +1,34 @@
-{ config, lib, pkgs, ... }:
+  { config, lib, pkgs, ... }:
 
-{
-  options.caddy.enable = lib.mkEnableOption "Enable Caddy";
+  {
+    options.caddy.enable = lib.mkEnableOption "Enable Caddy";
 
-  config = lib.mkIf config.caddy.enable {
+    config = lib.mkIf config.caddy.enable {
 
-    services.caddy = {
-      enable = true;
-      package = pkgs.caddy.withPlugins {
-        plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20240703190432-89f16b99c18e" ];
-        hash = "sha256-jCcSzenewQiW897GFHF9WAcVkGaS/oUu63crJu7AyyQ=";
+      services.caddy = {
+        enable = true;
+        package = pkgs.caddy.withPlugins {
+          plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20240703190432-89f16b99c18e" ];
+          hash = "sha256-jCcSzenewQiW897GFHF9WAcVkGaS/oUu63crJu7AyyQ=";
+        };
+
+        logDir = "/var/log/caddy";
+        dataDir = "/var/lib/caddy";
+
+        virtualHosts."valheim.hexaflare.net" = {
+          extraConfig = ''
+            reverse_proxy 192.168.1.32:2456
+            tls {
+                dns cloudflare {env.CF_API_TOKEN}
+            }
+          '';
+        };
       };
 
-      logDir = "/var/log/caddy";
-      dataDir = "/var/lib/caddy";
-
-      virtualHosts."hexaflare.net" = {
-        extraConfig = ''
-          reverse_proxy http://192.168.1.110:8081
-          tls {
-              dns cloudflare {env.CF_API_TOKEN}
-          }
-        '';
+          systemd.services.caddy.environment = {
+        CF_API_TOKEN = "84TpuQ36remqPbzEyeGIAkMUZPW2AkobZoKuRglT";
       };
-    };
 
-        systemd.services.caddy.environment = {
-      CF_API_TOKEN = "84TpuQ36remqPbzEyeGIAkMUZPW2AkobZoKuRglT";
+      networking.firewall.allowedTCPPorts = [ 443 ];
     };
-
-    networking.firewall.allowedTCPPorts = [ 443 80 8081 ];
-  };
-}
+  }
