ede15df4e629d7d4a89170b6782caba6a3e66b91
Waddenn <tpatelas@proton.me>
2025-02-09 15:16:19 +0100

    feat: update flake configuration by removing zen-browser and adding chromium; enhance caddy service configuration

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -6,11 +6,43 @@
   config = lib.mkIf config.caddy.enable {
 
   services.caddy = {
-    enable = true;
-    virtualHosts."calibre.hexaflare.net".extraConfig = ''
-      reverse_proxy http://192.168.1.122:8080
+    enable = true;  
+    logDir = "/var/log/caddy";  
+    dataDir = "/var/lib/caddy"; 
+    extraConfig = ''
+      {
+        debug
+      }
+      
+      hexaflare.net {
+        reverse_proxy 192.168.1.110:8081
+        tls {
+          dns cloudflare {env.CF_API_TOKEN}  
+        }
+      }
     '';
   };
+
+  # environment.variables = {
+  #   CF_API_TOKEN = "ton_api_token";  
+  # };
+
+  systemd.services.caddy = {
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+  };
+
+  services.caddy.package = pkgs.caddy.overrideAttrs (old: {
+    buildInputs = (old.buildInputs or []) ++ [
+      pkgs.go
+    ];
+    nativeBuildInputs = (old.nativeBuildInputs or []) ++ [
+      pkgs.xcaddy
+    ];
+    postInstall = (old.postInstall or "") + ''
+      xcaddy build --with github.com/caddy-dns/cloudflare
+    '';
+  });
 
   };
 }
