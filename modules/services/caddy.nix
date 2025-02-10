387c802170d9271d7b03170a95543849799b1d6e
Waddenn <tpatelas@proton.me>
2025-02-10 15:18:48 +0100

    fix: update Caddy service configuration to enable insecure TLS verification for reverse proxy

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -17,7 +17,11 @@
 
         virtualHosts."nextcloud.hexaflare.net" = {
           extraConfig = ''
-            reverse_proxy https://192.168.1.106
+            reverse_proxy https://192.168.1.106:443 {
+                transport http {
+                    tls_insecure_skip_verify
+                }
+            }
             tls {
                 dns cloudflare {env.CF_API_TOKEN}
             }
