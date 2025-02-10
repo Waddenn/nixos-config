2821e504df9392c14269768c43f1b7da5cbfb5cc
Waddenn <tpatelas@proton.me>
2025-02-10 15:12:44 +0100

    fix: update Caddy service configuration to use CF_API_TOKEN from secrets path

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -27,7 +27,7 @@
       };
 
       systemd.services.caddy.environment = {
-        CF_API_TOKEN = "${config.sops.secrets.cf-api-token.path}";
+        CF_API_TOKEN = "${config.sops.secrets.CF_API_TOKEN.path}";
       };
 
       networking.firewall.allowedTCPPorts = [ 443 ];
