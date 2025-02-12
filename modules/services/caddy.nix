f4c7e91745be5b3e872ae62a2dd81b7f326a21bd
waddenn <waddenn.github@gmail.com>
2025-02-12 13:11:17 +0100

    fix: update Caddy configuration for reverse proxy and authentication handling

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -1,39 +1,49 @@
-  { config, lib, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
-  {
-    options.caddy.enable = lib.mkEnableOption "Enable Caddy";
+{
+  options.caddy.enable = lib.mkEnableOption "Enable Caddy";
 
-    config = lib.mkIf config.caddy.enable {
+  config = lib.mkIf config.caddy.enable {
 
-      services.caddy = {
-        enable = true;
-        package = pkgs.caddy.withPlugins {
-          plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20240703190432-89f16b99c18e" ];
-          hash = "sha256-jCcSzenewQiW897GFHF9WAcVkGaS/oUu63crJu7AyyQ=";
-        };
+    services.caddy = {
+      enable = true;
+      package = pkgs.caddy.withPlugins {
+        plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20240703190432-89f16b99c18e" ];
+        hash = "sha256-jCcSzenewQiW897GFHF9WAcVkGaS/oUu63crJu7AyyQ=";
+      };
 
-        logDir = "/var/log/caddy";
-        dataDir = "/var/lib/caddy";
+      logDir = "/var/log/caddy";
+      dataDir = "/var/lib/caddy";
 
-        virtualHosts."nextcloud.hexaflare.net" = {
-          extraConfig = ''
+      virtualHosts."nextcloud.hexaflare.net" = {
+        extraConfig = ''
+          route {
+            reverse_proxy /outpost.goauthentik.io/* https://192.168.1.107
+
+            forward_auth https://192.168.1.107 {
+              uri /outpost.goauthentik.io/auth/caddy
+              copy_headers X-Authentik-Username X-Authentik-Groups X-Authentik-Entitlements X-Authentik-Email X-Authentik-Name X-Authentik-Uid X-Authentik-Jwt X-Authentik-Meta-Jwks X-Authentik-Meta-Outpost X-Authentik-Meta-Provider X-Authentik-Meta-App X-Authentik-Meta-Version
+              trusted_proxies private_ranges
+            }
+
             reverse_proxy https://192.168.1.106:443 {
-                transport http {
-                    tls_insecure_skip_verify
-                }
+              transport http {
+                tls_insecure_skip_verify
+              }
             }
-            tls {
-                dns cloudflare {env.CF_API_TOKEN}
-            }
-          '';
-        };
-        
-      };
+          }
 
-      systemd.services.caddy.environment = {
-        CF_API_TOKEN = "${config.sops.secrets.cf_api_token.path}";
+          tls {
+            dns cloudflare {env.CF_API_TOKEN}
+          }
+        '';
       };
-
-      networking.firewall.allowedTCPPorts = [ 443 ];
     };
-  }
+
+    systemd.services.caddy.environment = {
+      CF_API_TOKEN = "${config.sops.secrets.cf_api_token.path}";
+    };
+
+    networking.firewall.allowedTCPPorts = [ 443 ];
+  };
+}
