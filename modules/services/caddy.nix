6479674668a19521dc88928e8374fdf7cd788565
waddenn <waddenn.github@gmail.com>
2025-02-13 10:07:36 +0100

    fix: update Caddy configuration for nextcloud.hexaflare.net with simplified reverse proxy and forward_auth settings

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -15,57 +15,22 @@
       logDir = "/var/log/caddy";
       dataDir = "/var/lib/caddy";
 
-      virtualHosts."auth.hexaflare.net" = {
-        extraConfig = ''
-          tls {
-              dns cloudflare {env.CF_API_TOKEN}
-          }
-
-          route {
-              reverse_proxy /outpost.goauthentik.io/* http://192.168.1.107:80 {
-                  header_up X-Forwarded-Proto {scheme}
-                  header_up X-Forwarded-For {remote}
-                  header_up Host {host}
-                  header_up Connection "Upgrade"
-                  header_up Upgrade {upstream_http_upgrade}
-              }
-
-              forward_auth http://192.168.1.107:80 {
-                  uri /outpost.goauthentik.io/auth/caddy
-                  copy_headers X-Authentik-Username X-Authentik-Groups X-Authentik-Email X-Authentik-Uid X-Authentik-Jwt
-                  transport http {
-                      tls
-                      tls_insecure_skip_verify
-                  }
-              }
-
-              reverse_proxy http://192.168.1.107:80 {
-                  header_up X-Forwarded-Proto {scheme}
-                  header_up X-Forwarded-For {remote}
-                  header_up Host {host}
-                  header_up Connection "Upgrade"
-                  header_up Upgrade {upstream_http_upgrade}
-              }
-          }
-        '';
-      };
-
       virtualHosts."nextcloud.hexaflare.net" = {
         extraConfig = ''
           tls {
               dns cloudflare {env.CF_API_TOKEN}
           }
-
           route {
+              reverse_proxy /outpost.goauthentik.io/* http://192.168.1.107:80
+              forward_auth http://192.168.1.107:80 {
+                  uri /outpost.goauthentik.io/auth/caddy
+                  copy_headers X-Authentik-Username X-Authentik-Groups X-Authentik-Email X-Authentik-Uid X-Authentik-Jwt
+                  trusted_proxies private_ranges
+              }
               reverse_proxy https://192.168.1.106:443 {
                   transport http {
                       tls_insecure_skip_verify
                   }
-                  header_up X-Forwarded-Proto {scheme}
-                  header_up X-Forwarded-For {remote}
-                  header_up Host {host}
-                  header_up Connection "Upgrade"
-                  header_up Upgrade {upstream_http_upgrade}
               }
           }
         '';
