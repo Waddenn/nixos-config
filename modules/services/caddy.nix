85d0ccdc2702e647d7813e1292ecc9a0c19a6417
waddenn <waddenn.github@gmail.com>
2025-02-13 10:50:46 +0100

    fix: remove error logs related to TLS certificate acquisition in Caddy service

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -51,10 +51,3 @@
     networking.firewall.allowedTCPPorts = [ 443 80 ];
   };
 }
-Feb 13 09:43:18 caddy caddy[55420]: {"level":"error","ts":1739439798.973226,"logger":"tls.obtain","msg":"could not get certificate from issuer","identifier":"auth.hexaflare.net","issuer":"acme-v02.api.letsencrypt.org-directory","error":"[auth.hexaflare.net] solving challenges: presenting for challenge: adding temporary record for zone \"hexaflare.net.\": got error status: HTTP 400: [{Code:6003 Message:Invalid request headers ErrorChain:[{Code:6111 Message:Invalid format for Authorization header}]}] (order=https://acme-staging-v02.api.letsencrypt.org/acme/order/184241564/22585932244) (ca=https://acme-staging-v02.api.letsencrypt.org/directory)"}
-Feb 13 09:43:18 caddy caddy[55420]: {"level":"error","ts":1739439798.9732668,"logger":"tls.obtain","msg":"will retry","error":"[auth.hexaflare.net] Obtain: [auth.hexaflare.net] solving challenges: presenting for challenge: adding temporary record for zone \"hexaflare.net.\": got error status: HTTP 400: [{Code:6003 Message:Invalid request headers ErrorChain:[{Code:6111 Message:Invalid format for Authorization header}]}] (order=https://acme-staging-v02.api.letsencrypt.org/acme/order/184241564/22585932244) (ca=https://acme-staging-v02.api.letsencrypt.org/directory)","attempt":4,"retrying_in":300,"elapsed":306.165131977,"max_duration":2592000}
-Feb 13 09:47:21 caddy systemd[1]: Reloading Caddy...
-Feb 13 09:47:21 caddy caddy[108080]: {"level":"info","ts":1739440041.9002588,"msg":"using config from file","file":"/etc/caddy/caddy_config"}
-Feb 13 09:47:21 caddy caddy[108080]: Error: adapting config using caddyfile: subject does not qualify for certificate: '}'
-Feb 13 09:47:21 caddy systemd[1]: caddy.service: Control process exited, code=exited, status=1/FAILURE
-Feb 13 09:47:21 caddy systemd[1]: Reload failed for Caddy.
