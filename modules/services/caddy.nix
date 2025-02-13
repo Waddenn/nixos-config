1b4dc159c5328c6b64d151aa79fc79547b4f2933
waddenn <waddenn.github@gmail.com>
2025-02-13 09:56:50 +0100

    fix: update Caddy configuration for auth.hexaflare.net with enhanced reverse proxy and forward_auth settings

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -15,6 +15,41 @@
       logDir = "/var/log/caddy";
       dataDir = "/var/lib/caddy";
 
+      virtualHosts."auth.hexaflare.net" = {
+        extraConfig = ''
+          tls {
+              dns cloudflare {env.CF_API_TOKEN}
+          }
+
+          route {
+              reverse_proxy /outpost.goauthentik.io/* http://192.168.1.107:80 {
+                  header_up X-Forwarded-Proto {scheme}
+                  header_up X-Forwarded-For {remote}
+                  header_up Host {host}
+                  header_up Connection "Upgrade"
+                  header_up Upgrade {upstream_http_upgrade}
+              }
+
+              forward_auth http://192.168.1.107:80 {
+                  uri /outpost.goauthentik.io/auth/caddy
+                  copy_headers X-Authentik-Username X-Authentik-Groups X-Authentik-Email X-Authentik-Uid X-Authentik-Jwt
+                  transport http {
+                      tls
+                      tls_insecure_skip_verify
+                  }
+              }
+
+              reverse_proxy http://192.168.1.107:80 {
+                  header_up X-Forwarded-Proto {scheme}
+                  header_up X-Forwarded-For {remote}
+                  header_up Host {host}
+                  header_up Connection "Upgrade"
+                  header_up Upgrade {upstream_http_upgrade}
+              }
+          }
+        '';
+      };
+
       virtualHosts."nextcloud.hexaflare.net" = {
         extraConfig = ''
           tls {
@@ -22,33 +57,25 @@
           }
 
           route {
-              reverse_proxy /outpost.goauthentik.io/* http://192.168.1.107:80
-
-          forward_auth https://auth.hexaflare.net {
-              uri /outpost.goauthentik.io/auth/caddy
-              copy_headers X-Authentik-Username X-Authentik-Groups X-Authentik-Email X-Authentik-Uid X-Authentik-Jwt
-              transport http {
-        tls
-        tls_insecure_skip_verify
-    }
-          }
-
-
               reverse_proxy https://192.168.1.106:443 {
                   transport http {
                       tls_insecure_skip_verify
                   }
+                  header_up X-Forwarded-Proto {scheme}
+                  header_up X-Forwarded-For {remote}
+                  header_up Host {host}
+                  header_up Connection "Upgrade"
+                  header_up Upgrade {upstream_http_upgrade}
               }
           }
         '';
       };
-
     };
 
     systemd.services.caddy.environment = {
       CF_API_TOKEN = "${config.sops.secrets.cf_api_token.path}";
     };
 
-    networking.firewall.allowedTCPPorts = [ 443 ];
+    networking.firewall.allowedTCPPorts = [ 443 80 ];
   };
 }
