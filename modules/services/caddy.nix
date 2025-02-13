2f4290c3321173963047d480083ae6295c6997f1
waddenn <waddenn.github@gmail.com>
2025-02-13 13:49:57 +0100

    fix: update reverse proxy configuration to use HTTPS for Auth service

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -21,10 +21,11 @@
               dns cloudflare {env.CF_API_TOKEN}
           }
           route {
-              reverse_proxy /outpost.goauthentik.io/* http://192.168.1.107:80
-              forward_auth http://192.168.1.107:80 {
+              reverse_proxy /outpost.goauthentik.io/* https://192.168.1.107:9443
+              forward_auth https://192.168.1.107:9443 {
                   uri /outpost.goauthentik.io/auth/caddy
-                copy_headers X-Authentik-Username X-Authentik-Groups X-Authentik-Email X-Authentik-Name X-Authentik-Uid X-Authentik-Jwt X-Authentik-Meta-Jwks X-Authentik-Meta-Outpost X-Authentik-Meta-Provider X-Authentik-Meta-App X-Authentik-Meta-Version X-Forwarded-Host Authorization                  trusted_proxies private_ranges
+                copy_headers X-Authentik-Username X-Authentik-Groups X-Authentik-Email X-Authentik-Name X-Authentik-Uid X-Authentik-Jwt X-Authentik-Meta-Jwks X-Authentik-Meta-Outpost X-Authentik-Meta-Provider X-Authentik-Meta-App X-Authentik-Meta-Version X-Forwarded-Host Authorization                  
+                :trusted_proxies private_ranges
               }
               reverse_proxy https://192.168.1.106:443 {
                   transport http {
