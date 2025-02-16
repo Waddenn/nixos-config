c5c41612f9e405eec54c860cafa1cb9873bad8a7
Waddenn <tpatelas@proton.me>
2025-02-16 16:43:44 +0100

    fix: update cf_api_token in secrets.yaml and remove deprecated secrets.json

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -1,37 +1,37 @@
-{ config, lib, pkgs, ... }:
+  { config, lib, pkgs, ... }:
 
-{
-  options.caddy.enable = lib.mkEnableOption "Enable Caddy";
+  {
+    options.caddy.enable = lib.mkEnableOption "Enable Caddy";
 
-  config = lib.mkIf config.caddy.enable {
+    config = lib.mkIf config.caddy.enable {
 
-    services.caddy = {
-      enable = true;
-      package = pkgs.caddy.withPlugins {
-        plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20240703190432-89f16b99c18e" ];
-        hash = "sha256-JVkUkDKdat4aALJHQCq1zorJivVCdyBT+7UhqTvaFLw=";
+      services.caddy = {
+        enable = true;
+        package = pkgs.caddy.withPlugins {
+          plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20240703190432-89f16b99c18e" ];
+          hash = "sha256-JVkUkDKdat4aALJHQCq1zorJivVCdyBT+7UhqTvaFLw=";
+        };
+
+        logDir = "/var/log/caddy";
+        dataDir = "/var/lib/caddy";
+
+        virtualHosts."test.hexaflare.net" = {
+          extraConfig = ''
+            tls {
+              dns cloudflare {env.CF_API_TOKEN}
+            }
+            reverse_proxy http://192.168.1.107:80 {
+            }
+          '';
+        };
+
       };
 
-      logDir = "/var/log/caddy";
-      dataDir = "/var/lib/caddy";
-
-      virtualHosts."myspeed.hexaflare.net" = {
-        extraConfig = ''
-          tls {
-            dns cloudflare {env.CF_API_TOKEN}
-          }
-          reverse_proxy http://192.168.1.107:80 {
-          }
-        '';
+      systemd.services.caddy.environment = {
+        CF_API_TOKEN = "${config.sops.secrets.cf_api_token}";
       };
 
+      networking.firewall.allowedTCPPorts = [ 443 80 ];
     };
-
-    systemd.services.caddy.environment = {
-      CF_API_TOKEN = "${config.sops.secrets.cf_api_token.path}";
-    };
-
-    networking.firewall.allowedTCPPorts = [ 443 80 ];
-  };
-}
+  }
 
