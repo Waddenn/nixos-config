8ce93e2731394399b8ffd7a031afc3e3fc550f62
waddenn <waddenn.github@gmail.com>
2025-02-16 17:03:35 +0100

    fix: update Caddy configuration to use EnvironmentFile for CLOUDFLARE_API_TOKEN

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -1,35 +1,38 @@
-  { config, lib, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
-  {
-    options.caddy.enable = lib.mkEnableOption "Enable Caddy";
+{
+  options.caddy.enable = lib.mkEnableOption "Enable Caddy";
 
-    config = lib.mkIf config.caddy.enable {
+  config = lib.mkIf config.caddy.enable {
 
-      services.caddy = {
-        enable = true;
-        package = pkgs.caddy.withPlugins {
-          plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20240703190432-89f16b99c18e" ];
-          hash = "sha256-JVkUkDKdat4aALJHQCq1zorJivVCdyBT+7UhqTvaFLw=";
-        };
-
-        logDir = "/var/log/caddy";
-        dataDir = "/var/lib/caddy";
-
-        virtualHosts."test.hexaflare.net" = {
-          extraConfig = ''
-            tls {
-              dns cloudflare {$CLOUDFLARE_API_TOKEN}
-            }
-            reverse_proxy http://192.168.1.107:80 {
-            }
-          '';
-        };
-
+    services.caddy = {
+      enable = true;
+      package = pkgs.caddy.withPlugins {
+        plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20240703190432-89f16b99c18e" ];
+        hash = "sha256-JVkUkDKdat4aALJHQCq1zorJivVCdyBT+7UhqTvaFLw=";
       };
-    systemd.services.caddy = {
-      serviceConfig.Environment = "CLOUDFLARE_API_TOKEN=${config.sops.secrets.cloudflare_token}";
-    };
-      networking.firewall.allowedTCPPorts = [ 443 80 ];
-    };
-  }
 
+      logDir = "/var/log/caddy";
+      dataDir = "/var/lib/caddy";
+
+      virtualHosts."test.hexaflare.net" = {
+        extraConfig = ''
+          tls {
+            dns cloudflare {$CLOUDFLARE_API_TOKEN}
+          }
+          reverse_proxy http://192.168.1.107:80 {
+          }
+        '';
+      };
+    };
+
+
+    systemd.services.caddy = {
+      serviceConfig = {
+        EnvironmentFile = config.sops.secrets.cloudflare_token.path;
+      };
+    };
+
+    networking.firewall.allowedTCPPorts = [ 443 80 ];
+  };
+}
