36174472382c9095714984944c405d0f3838921e
waddenn <waddenn.github@gmail.com>
2025-02-16 15:21:13 +0100

    fix: update Caddy configuration to remove outdated forward auth settings and adjust firewall ports

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -17,25 +17,15 @@
 
       virtualHosts."nextcloud.hexaflare.net" = {
         extraConfig = ''
-          route {
-              reverse_proxy /outpost.goauthentik.io/* http://192.168.1.107:80
-              forward_auth http://192.168.1.107:80 {
-                  uri /outpost.goauthentik.io/auth/caddy
-                  header X-Authentik-Username {http.request.header.X-Authentik-Username}
-                  header X-Authentik-Groups {http.request.header.X-Authentik-Groups}
-                  header X-Authentik-Email {http.request.header.X-Authentik-Email}
-                  header X-Authentik-Uid {http.request.header.X-Authentik-Uid}
-                  header X-Authentik-Jwt {http.request.header.X-Authentik-Jwt}
-                  trusted_proxies private_ranges
-              }
-              reverse_proxy https://192.168.1.106:443 {
-                  transport http {
-                      tls_insecure_skip_verify
-                  }
-              }
-          }
           tls {
               dns cloudflare {env.CF_API_TOKEN}
+            }
+
+           reverse_proxy https://192.168.1.106:443 {
+               transport http {
+                   tls_insecure_skip_verify
+               }
+           }
           }
         '';
       };
@@ -46,6 +36,6 @@
       CF_API_TOKEN = "${config.sops.secrets.cf_api_token.path}";
     };
 
-    networking.firewall.allowedTCPPorts = [ 443 ];
+    networking.firewall.allowedTCPPorts = [ 443 80 ];
   };
 }
