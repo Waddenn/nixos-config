d022a0117220a1ae51636cedc27bc07ebcc91461
waddenn <waddenn.github@gmail.com>
2025-02-16 16:10:09 +0100

    fix: update Caddy configuration to change reverse proxy settings

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -20,7 +20,10 @@
           tls {
             dns cloudflare {env.CF_API_TOKEN}
           }
-          reverse_proxy http://192.168.1.104:5216 {
+          reverse_proxy https://192.168.1.106:443 {
+            transport http {
+              tls_insecure_skip_verify
+            }
           }
         '';
       };
