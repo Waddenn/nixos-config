b45db009e723876b98f215d91ab8169dcadff7e8
waddenn <waddenn.github@gmail.com>
2025-02-16 17:36:17 +0100

    fix: update Caddy configuration to use cf_api_token from secrets.yaml

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -1,35 +1,37 @@
-{ config, lib, pkgs, ... }:
+  { config, lib, pkgs, ... }:
 
-{
-  options.caddy.enable = lib.mkEnableOption "Enable Caddy";
+  {
+    options.caddy.enable = lib.mkEnableOption "Enable Caddy";
 
-  config = lib.mkIf config.caddy.enable {
+    config = lib.mkIf config.caddy.enable {
 
-    services.caddy = {
-      enable = true;
-      package = pkgs.caddy.withPlugins {
-        plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20240703190432-89f16b99c18e" ];
-        hash = "sha256-JVkUkDKdat4aALJHQCq1zorJivVCdyBT+7UhqTvaFLw=";
+      services.caddy = {
+        enable = true;
+        package = pkgs.caddy.withPlugins {
+          plugins = [ "github.com/caddy-dns/cloudflare@v0.0.0-20240703190432-89f16b99c18e" ];
+          hash = "sha256-JVkUkDKdat4aALJHQCq1zorJivVCdyBT+7UhqTvaFLw=";
+        };
+
+        logDir = "/var/log/caddy";
+        dataDir = "/var/lib/caddy";
+
+      extraEnvironment = {
+        CF_API_TOKEN = builtins.readFile (sopsFile ./secrets/cf_api_token.sops);
       };
 
-      logDir = "/var/log/caddy";
-      dataDir = "/var/lib/caddy";
+        virtualHosts."test.hexaflare.net" = {
+          extraConfig = ''
+            tls {
+              dns cloudflare {env.CF_API_TOKEN}
+            }
+            reverse_proxy http://192.168.1.107:80 {
+            }
+          '';
+        };
 
-      virtualHosts."test.hexaflare.net" = {
-        extraConfig = ''
-          tls {
-            dns cloudflare env:CLOUDFLARE_API_KEY
-          }
-          reverse_proxy http://192.168.1.107:80 {
-          }
-        '';
       };
 
+      networking.firewall.allowedTCPPorts = [ 443 80 ];
     };
+  }
 
-    networking.firewall.allowedTCPPorts = [ 443 80 ];
-
-    systemd.services.caddy.serviceConfig.EnvironmentFile = "/etc/environment";
-
-  };
-}
