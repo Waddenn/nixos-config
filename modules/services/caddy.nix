1cfdd08a00e779d54d10815203a532f669b2e6be
Waddenn <tpatelas@proton.me>
2025-02-16 18:06:43 +0100

    fix: update reverse proxy configuration for Nextcloud virtual host

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -19,11 +19,11 @@
         dataDir = "/var/lib/caddy";
         environmentFile  = config.sops.secrets.cf_api_token.path;
 
-
+        
       virtualHosts."nextcloud.hexaflare.net" = {
         extraConfig = ''
           route {
-            reverse_proxy /outpost.goauthentik.io/* http://192.168.1.107:80
+            reverse_proxy /outpost.goauthentik.io/* auth.hexaflare.net
 
             forward_auth auth.hexaflare.net {
                 uri /outpost.goauthentik.io/auth/caddy
