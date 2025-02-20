e09734534f7218e56c57eb4a9f9e64a872a82500
waddenn <waddenn.github@gmail.com>
2025-02-20 22:05:38 +0100

    feat: update Vaultwarden service configuration to use localhost and add Caddy reverse proxy

--- modules/services/vaultwarden.nix
+++ modules/services/vaultwarden.nix
@@ -1,15 +1,33 @@
 { config, lib, pkgs, ... }:
 
 {
-  options.vaultwarden.enable = lib.mkEnableOption "Enable vaultwarden";
+  options.vaultwarden.enable = lib.mkEnableOption "Enable Vaultwarden";
 
   config = lib.mkIf config.vaultwarden.enable {
 
-  services.vaultwarden.enable = true;
-  services.vaultwarden.config = {
-      ROCKET_ADDRESS = "0.0.0.0"; 
-      ROCKET_PORT = "8222"; 
-  };
-  
+    services.vaultwarden = {
+      enable = true;
+      config = {
+        ROCKET_ADDRESS = "127.0.0.1";  
+        ROCKET_PORT = "8222"; 
+      };
+    };
+
+    services.caddy = {
+      enable = true;
+      virtualHosts = {
+        "vaultwarden.local" = {
+          extraConfig = ''
+            reverse_proxy http://127.0.0.1:8222
+            tls internal  # Utilise un certificat auto-signé de Caddy
+          '';
+        };
+      };
+    };
+
+    networking.extraHosts = ''
+      127.0.0.1 vaultwarden.local
+    '';
+
   };
 }
