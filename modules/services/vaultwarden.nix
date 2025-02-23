2a8264f6693ba7e72ee9ade5969fe0a37ea3872b
Waddenn <tpatelas@proton.me>
2025-02-23 10:11:56 +0100

    feat: update Vaultwarden service configuration to allow traffic on port 443 and bind to all interfaces

--- modules/services/vaultwarden.nix
+++ modules/services/vaultwarden.nix
@@ -8,26 +8,10 @@
     services.vaultwarden = {
       enable = true;
       config = {
-        ROCKET_ADDRESS = "127.0.0.1";  
+        ROCKET_ADDRESS = "0.0.0.0";  
         ROCKET_PORT = "8222"; 
       };
     };
-
-    services.caddy = {
-      enable = true;
-      virtualHosts = {
-        "vaultwarden.local" = {
-          extraConfig = ''
-            reverse_proxy http://127.0.0.1:8222
-            tls internal  
-          '';
-        };
-      };
-    };
-
-    networking.extraHosts = ''
-      127.0.0.1 vaultwarden.local
-    '';
 
   };
 }
