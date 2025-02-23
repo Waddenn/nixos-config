016aa1486e8319d679322c7ad2d056c5a5f8516e
waddenn <waddenn.github@gmail.com>
2025-02-23 10:11:56 +0100

    feat: update Vaultwarden service configuration to allow traffic on port 443 and bind to all interfaces

--- flake.nix
+++ flake.nix
@@ -189,7 +189,7 @@
           modules = [
             {
               vaultwarden.enable = true;
-              networking.firewall.allowedTCPPorts = [ 8222 ];
+              networking.firewall.allowedTCPPorts = [ 443 8222 ];
             }
           ];
         });
