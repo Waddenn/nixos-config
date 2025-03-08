6c5136e7491b8cc9500d2e761c52203fd5ca6ab2
Waddenn <tpatelas@proton.me>
2025-03-08 20:59:42 +0100

    fix: update flake.lock and modify favorite apps; switch from Firefox to Brave browser

--- modules/boot/kernel.nix
+++ modules/boot/kernel.nix
@@ -4,7 +4,7 @@
   options.linuxPackages_latest.enable = lib.mkEnableOption "linuxPackages_latest";
 
   config = lib.mkIf config.linuxPackages_latest.enable {
-    boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_11;
+    boot.kernelPackages = pkgs.linuxPackages_latest;
 
   };
 }
