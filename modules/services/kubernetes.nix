71b7d5541c400e806ebe65aa2b0040176cfe5965
waddenn <waddenn.github@gmail.com>
2025-03-12 14:10:53 +0100

    feat: add Kubernetes service configuration options

---
+++ modules/services/kubernetes.nix
@@ -0,0 +1,11 @@
+{ config, lib, ... }:
+
+{
+  options.kubernetes.enable = lib.mkEnableOption "Enable kubernetes";
+
+  config = lib.mkIf config.kubernetes.enable {
+
+  services.kubernetes.roles = [ "master" "node" ];
+
+  };
+}
