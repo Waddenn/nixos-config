0b463d13fae73761105a4d92a8fb112bee50af7f
Waddenn <tpatelas@proton.me>
2025-03-14 11:46:11 +0100

    refactor: remove Caddy reverse proxy configuration for OnlyOffice service

--- modules/services/onlyoffice.nix
+++ modules/services/onlyoffice.nix
@@ -10,17 +10,7 @@
     hostname = "localhost";
   };
   networking.firewall.allowedTCPPorts = [ 8000 ];
-
-  services.caddy = {
-  virtualHosts = {
-    "office.example.org".extraConfig = ''
-      reverse_proxy http://127.0.0.1:8000 {
-        # Required to circumvent bug of Onlyoffice loading mixed non-https content
-        header_up X-Forwarded-Proto https
-      }
-    '';
-  };
-};
+  
   };
 }
 
