97469fcfb94fee6626ecf3597f1b3a9f6f320836
waddenn <waddenn.github@gmail.com>
2025-04-08 18:26:03 +0200

    feat: add hydenix configuration and modules for enhanced user experience

--- home-manager/tom/home.nix
+++ home-manager/tom/home.nix
@@ -2,13 +2,13 @@
 
 {
   imports = [
-    ./home/packages.nix
-    ./programs/git.nix
-    ./desktopManager/gnome/background.nix
-    ./desktopManager/gnome/extensions.nix
-    ./desktopManager/gnome/favorite-apps.nix
-    ./desktopManager/gnome/interface.nix
-    ./desktopManager/gnome/keybindings.nix
+    # ./home/packages.nix
+    # ./programs/git.nix
+    # ./desktopManager/gnome/background.nix
+    # ./desktopManager/gnome/extensions.nix
+    # ./desktopManager/gnome/favorite-apps.nix
+    # ./desktopManager/gnome/interface.nix
+    # ./desktopManager/gnome/keybindings.nix
   ];
   
 
@@ -18,6 +18,91 @@
   home.stateVersion = "24.05";
 
   programs.home-manager.enable = true;
+
+  {
+  hydenix.hm = {
+
+    #! Important options
+    enable = true;
+
+    #! Below are defaults
+    comma.enable = true; # useful nix tool to run software without installing it first
+    dolphin.enable = true; # file manager
+    editors = {
+      enable = true; # enable editors module
+      neovim.enable = true; # enable neovim module
+      vscode = {
+        enable = true; # enable vscode module
+        wallbash = true; # enable wallbash extension for vscode
+      };
+      vim.enable = true; # enable vim module
+      default = "nano"; # default text editor
+    };
+    fastfetch.enable = true; # fastfetch configuration
+    firefox = {
+      enable = true; # enable firefox module
+      useHydeConfig = false; # use hyde firefox configuration and extensions
+      useUserChrome = true; # if useHydeConfig is true, apply hyde userChrome CSS customizations
+      useUserJs = true; # if useHydeConfig is true, apply hyde user.js preferences
+      useExtensions = true; # if useHydeConfig is true, install hyde firefox extensions
+    };
+    gaming.enable = true; # enable gaming module
+    git = {
+      enable = true; # enable git module
+      name = "waddenn"; # git user name eg "John Doe"
+      email = "waddenn.github@gmail.com"; # git user email eg "john.doe@example.com"
+    };
+    hyde.enable = true; # enable hyde module
+    hyprland.enable = true; # enable hyprland module
+    lockscreen = {
+      enable = true; # enable lockscreen module
+      hyprlock = true; # enable hyprlock lockscreen
+      swaylock = false; # enable swaylock lockscreen
+    };
+    notifications.enable = true; # enable notifications module
+    qt.enable = true; # enable qt module
+    rofi.enable = true; # enable rofi module
+    screenshots = {
+      enable = true; # enable screenshots module
+      grim.enable = true; # enable grim screenshot tool
+      slurp.enable = true; # enable slurp region selection tool
+      satty.enable = true; # enable satty screenshot annotation tool
+      swappy.enable = false; # enable swappy screenshot editor
+    };
+    wallpapers.enable = true; # enable wallpapers module
+    shell = {
+      enable = true; # enable shell module
+      zsh.enable = true; # enable zsh shell
+      configText = ""; # zsh config text
+      bash.enable = false; # enable bash shell
+      fish.enable = false; # enable fish shell
+      pokego.enable = true; # enable Pokemon ASCII art scripts
+    };
+    social = {
+      enable = true; # enable social module
+      discord.enable = true; # enable discord module
+      webcord.enable = true; # enable webcord module
+      vesktop.enable = true; # enable vesktop module
+    };
+    spotify.enable = true; # enable spotify module
+    swww.enable = true; # enable swww wallpaper daemon
+    terminals = {
+      enable = true; # enable terminals module
+      kitty = {
+        enable = true; # enable kitty terminal
+        configText = ""; # kitty config text
+      };
+    };
+    theme = {
+      enable = true; # enable theme module
+      active = "Catppuccin Mocha"; # active theme name
+      themes = [ "Catppuccin Mocha" "Catppuccin Latte" ]; # default enabled themes, full list in https://github.com/richen604/hydenix/tree/main/hydenix/sources/themes
+    };
+    waybar.enable = true; # enable waybar module
+    wlogout.enable = true; # enable wlogout module
+    xdg.enable = true; # enable xdg module
+  };
+}
   
 
 }
--- hosts/asus-nixos/configuration.nix
+++ hosts/asus-nixos/configuration.nix
@@ -5,53 +5,80 @@
   
   imports = [
     ../../modules/global.nix
-    ../../users/hypr/default.nix
   ];
   
-  environment.systemPackages = with pkgs; [
-    age
-    sops
-    just
-    mesa
-  ];
+  # environment.systemPackages = with pkgs; [
+  #   age
+  #   sops
+  #   just
+  #   mesa
+  # ];
 
-  services.flatpak.packages = [
-    "tv.plex.PlexDesktop"
-    "org.fedoraproject.MediaWriter"
-    "dev.vencord.Vesktop"
-    "com.github.taiko2k.avvie"
-    "com.boxy_svg.BoxySVG"
-  ];
+  # services.flatpak.packages = [
+  #   "tv.plex.PlexDesktop"
+  #   "org.fedoraproject.MediaWriter"
+  #   "dev.vencord.Vesktop"
+  #   "com.github.taiko2k.avvie"
+  #   "com.boxy_svg.BoxySVG"
+  # ];
 
-  autoUpgrade.enable = true;
-  firefox.enable = true;
-  bluetooth.enable = true;
-  systemd-boot.enable = true;
-  keyMap.enable = true;
-  i18n.enable = true;
-  networkmanager.enable = true;
-  tailscale-client.enable = true;
-  gc.enable = true;
-  experimental-features.enable = true;
-  allowUnfree.enable = true;
-  direnv.enable = true;
-  steam.enable = true;
-  zsh.enable = true;
-  rtkit.enable = true;
-  timeZone.enable = true;
-  zram.enable = true;
-  gnome-excludePackages.enable = true;
-  gnomeExtensions.enable = true;
-  flatpak.enable = true;
-  printing.enable = true;
-  pipewire.enable = true;
-  fprintd.enable = false;
-  fwupd.enable = true;
-  xkb.enable = true;
-  gnome.enable = true;
-  gdm.enable = true;
-  docker.enable = true;
-  hyprland.enable = true;
+  # autoUpgrade.enable = true;
+  # firefox.enable = true;
+  # bluetooth.enable = true;
+  # systemd-boot.enable = true;
+  # keyMap.enable = true;
+  # i18n.enable = true;
+  # networkmanager.enable = true;
+  # tailscale-client.enable = true;
+  # gc.enable = true;
+  # experimental-features.enable = true;
+  # allowUnfree.enable = true;
+  # direnv.enable = true;
+  # steam.enable = true;
+  # zsh.enable = true;
+  # rtkit.enable = true;
+  # timeZone.enable = true;
+  # zram.enable = true;
+  # gnome-excludePackages.enable = true;
+  # gnomeExtensions.enable = true;
+  # flatpak.enable = true;
+  # printing.enable = true;
+  # pipewire.enable = true;
+  # fprintd.enable = false;
+  # fwupd.enable = true;
+  # xkb.enable = true;
+  # gnome.enable = true;
+  # gdm.enable = true;
+  # docker.enable = true;
+  # hyprland.enable = true;
 
-  linuxPackages.enable6_6 = true;
+  # linuxPackages.enable6_6 = true;
+
+    hydenix = {
+
+    #! Important options
+    enable = true; # enable hydenix - required, default false
+    hostname = "hydenix"; # hostname
+    timezone = "Europe/Paris"; # timezone
+    locale = "en_US.UTF-8"; # locale
+
+    #! Below are defaults
+    audio.enable = true; # enable audio module
+    boot = {
+      enable = true; # enable boot module
+      useSystemdBoot = true; # disable for GRUB
+      grubTheme = pkgs.hydenix.grub-retroboot; # or pkgs.hydenix.grub-pochita
+      grubExtraConfig = ""; # additional GRUB configuration
+      kernelPackages = pkgs.linuxPackages_zen; # default zen kernel
+    };
+    gaming.enable = true; # enable gaming module
+    hardware.enable = true; # enable hardware module
+    network.enable = true; # enable network module
+    nix.enable = true; # enable nix module
+    sddm = {
+      enable = true; # enable sddm module
+      theme = pkgs.hydenix.sddm-candy; # or pkgs.hydenix.sddm-corners
+    };
+    system.enable = true; # enable system module
+  };
 } 
