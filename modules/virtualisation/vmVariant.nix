f2c1b747327d165b407a63412cf9b48fd5f82014
waddenn <waddenn.github@gmail.com>
2025-04-15 20:11:07 +0200

    feat: add VM configuration for memory size and CPU cores in vmVariant.nix

---
+++ modules/virtualisation/vmVariant.nix
@@ -0,0 +1,7 @@
+  virtualisation.vmVariant = {
+    # following configuration is added only when building VM with build-vm
+    virtualisation = {
+      memorySize = 6048; # Use 4048MiB memory.
+      cores = 6;         # Use 6 CPU cores.
+    };
+  };
