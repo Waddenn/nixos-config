d7d550215adc13248d1951d3957588170e8d665e
Waddenn <tpatelas@proton.me>
2025-04-22 18:10:30 +0200

    refactor: move mkDesktopSystem and mkProxmoxSystem definitions to flake.nix and remove mkSystems.nix

--- flake.nix
+++ flake.nix
@@ -14,7 +14,45 @@
   outputs = inputs@{ self, nixpkgs, home-manager, nix-flatpak, sops-nix, ... }:
     let
       lib = nixpkgs.lib;
-      mkSystems = import ./lib/mkSystems.nix { inherit inputs nixpkgs home-manager; };
+
+      mkDesktopSystem = { hostname, username }:
+        {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs nixpkgs home-manager username; };
+          modules = [
+            ./hosts/${hostname}/hardware-configuration.nix
+            ./hosts/${hostname}/configuration.nix
+            ./users/${username}/default.nix
+            home-manager.nixosModules.home-manager
+            inputs.nix-flatpak.nixosModules.nix-flatpak
+            inputs.sops-nix.nixosModules.sops
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.${username} = import ./home-manager/${username}/home.nix;
+              sops.defaultSopsFile = ./secrets/secrets.yaml;
+              sops.age.sshKeyPaths = [ "/home/tom/.ssh/id_ed25519" ];
+              networking.hostName = hostname;
+              system.stateVersion = "25.05";
+            }
+          ];
+        };
+
+      mkProxmoxSystem = { hostname, username }:
+        {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs nixpkgs username; };
+          modules = [
+            ./hosts/proxmox-vm/hardware-configuration.nix
+            ./hosts/proxmox-vm/configuration.nix
+            ./users/${username}/default.nix
+            inputs.sops-nix.nixosModules.sops
+            {
+              networking.hostName = hostname;
+              system.stateVersion = "25.05";
+            }
+          ];
+        };
 
       commonServerModules = [
         ./modules/global.nix
@@ -31,10 +69,7 @@
       ];
 
       mkDesktop = name: username:
-        lib.nixosSystem (mkSystems.mkDesktopSystem {
-          hostname = name;
-          username = username;
-        });
+        lib.nixosSystem (mkDesktopSystem { hostname = name; username = username; });
 
       mkServer = name: extraModules:
         lib.nixosSystem {
@@ -166,7 +201,7 @@
           { nextcloud.enable = true; }
         ];
 
-        kubernetes = lib.nixosSystem (mkSystems.mkProxmoxSystem {
+        kubernetes = lib.nixosSystem (mkProxmoxSystem {
           hostname = "kubernetes";
           username = "nixos";
         });
