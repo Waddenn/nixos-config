19cae0492c2fb46f38a0c73af2cdcfc354400a71
waddenn <waddenn.github@gmail.com>
2025-04-24 19:06:22 +0200

    fix: restore configuration structure for GitHub runner service

--- modules/services/github-runner.nix
+++ modules/services/github-runner.nix
@@ -1,19 +1,20 @@
-# {
-#   config,
-#   lib,
-#   pkgs,
-#   ...
-# }: let
-#   githubToken = "";
-#   githubRepo = "tom/nixos-config";
-# in {
-#   services.github-runners = {
-#     runner1 = {
-#       replace = true;
-#       token = githubToken;
-#       url = "https://github.com/${githubRepo}";
-#       extraLabels = ["nixos" "self-hosted"];
-#       workDir = "/var/lib/github-runner";
-#     };
-#   };
-# }
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  options.services.github-runner.enable = lib.mkEnableOption "Enable GitHub Actions runner";
+
+  config = lib.mkIf config.services.github-runner.enable {
+    services.github-runners = {
+      runner1 = {
+        replace = true;
+        # token = "";
+        url = "https://github.com/tom/nixos-config";
+        extraLabels = ["nixos" "self-hosted"];
+        workDir = "/var/lib/github-runner";
+      };
+    };
+  };
+}
