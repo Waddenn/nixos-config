4dca6545718da1a6c97b5302a61458e8de6234a2
Waddenn <tpatelas@proton.me>
2025-04-24 19:02:51 +0200

    fix: update GitHub runner service configuration to use correct structure

--- modules/services/github-runner.nix
+++ modules/services/github-runner.nix
@@ -7,12 +7,13 @@
   githubToken = "";
   githubRepo = "tom/nixos-config";
 in {
-  services.github-runner = {
-    enable = true;
-    name = "runner1";
-    token = githubToken;
-    url = "https://github.com/${githubRepo}";
-    extraLabels = ["nixos" "self-hosted"];
-    workDir = "/var/lib/github-runner";
+  services.github-runners = {
+    runner1 = {
+      replace = true;
+      token = githubToken;
+      url = "https://github.com/${githubRepo}";
+      extraLabels = ["nixos" "self-hosted"];
+      workDir = "/var/lib/github-runner";
+    };
   };
 }
