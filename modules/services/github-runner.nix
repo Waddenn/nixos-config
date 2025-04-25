357698236d994546a501dcda5e5333dcd62d5734
waddenn <waddenn.github@gmail.com>
2025-04-25 12:23:58 +0200

    fix: update token file path and service overrides for GitHub runner configuration

--- modules/services/github-runner.nix
+++ modules/services/github-runner.nix
@@ -11,10 +11,13 @@
       nixos-runner = {
         enable = true;
         replace = true;
-        tokenFile = "/etc/secrets/github-runner/nixos.token";
+        tokenFile = "/var/lib/github-runner/nixos-runner/.token";
         url = "https://github.com/Waddenn/nixos-config";
         extraLabels = ["nixos" "self-hosted"];
         workDir = "/var/lib/github-runner/nixos-runner";
+        serviceOverrides = {
+          StateDirectory = "github-runner/nixos-runner";
+        };
       };
     };
   };
