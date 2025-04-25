971b064a174acab2e40bdc5b57cd198002766977
Waddenn <tpatelas@proton.me>
2025-04-25 11:28:37 +0200

    fix: add systemd tmpfiles rules for GitHub runner directory

--- modules/services/github-runner.nix
+++ modules/services/github-runner.nix
@@ -7,10 +7,16 @@
   options.githubRunner.enable = lib.mkEnableOption "Enable GitHub Actions runner";
 
   config = lib.mkIf config.githubRunner.enable {
+    systemd.tmpfiles.rules = [
+      "d /var/lib/github-runner 0750 github-runner github-runner -"
+    ];
+
     services.github-runners = {
       nixos-runner = {
         enable = true;
         replace = true;
+        user = "github-runner";
+        group = "github-runner";
         tokenFile = "/run/secrets/github-runner/nixos.token";
         url = "https://github.com/Waddenn/nixos-config";
         extraLabels = ["nixos" "self-hosted"];
