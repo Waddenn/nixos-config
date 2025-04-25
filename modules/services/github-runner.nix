6a81260d907308c669f814d2d02f8855830d8f20
waddenn <waddenn.github@gmail.com>
2025-04-25 13:17:30 +0200

    fix: update token file path for GitHub Actions runner configuration

--- modules/services/github-runner.nix
+++ modules/services/github-runner.nix
@@ -3,17 +3,26 @@
   lib,
   pkgs,
   ...
-}: let
-  tokenPath = "/secrets/github-runner.token";
-in {
+}: {
   options.githubRunner.enable = lib.mkEnableOption "Enable GitHub Actions runner";
 
   config = lib.mkIf config.githubRunner.enable {
+    # 1) on importe le token depuis un fichier hors store (ici : /root/github-runner.token)
+    environment.etc = {
+      "secrets/github-runner.token" = {
+        source = /root/github-runner.token;
+        user = "root";
+        group = "root";
+        mode = "0400";
+      };
+    };
+
+    # 2) on pointe le runner dessus
     services.github-runners = {
       nixos-runner = {
         enable = true;
         url = "https://github.com/Waddenn/nixos-config";
-        tokenFile = tokenPath;
+        tokenFile = "/etc/secrets/github-runner.token";
         extraLabels = ["nixos" "self-hosted"];
         package = pkgs.github-runner;
 
