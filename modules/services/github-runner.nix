35865b3c23eb19a14303ab8389541c6ae419017b
Waddenn <tpatelas@proton.me>
2025-04-25 11:41:05 +0200

    fix: simplify GitHub runner configuration by removing user and group definitions

--- modules/services/github-runner.nix
+++ modules/services/github-runner.nix
@@ -7,36 +7,14 @@
   options.githubRunner.enable = lib.mkEnableOption "Enable GitHub Actions runner";
 
   config = lib.mkIf config.githubRunner.enable {
-    users.groups.github-runner = {};
-    users.users.github-runner = {
-      isSystemUser = true;
-      group = "github-runner";
-      home = "/var/lib/github-runner";
-      createHome = true;
-      shell = pkgs.bashInteractive;
-    };
-
-    systemd.tmpfiles.rules = [
-      "d /var/lib/github-runner 0750 github-runner github-runner -"
-      "d /var/lib/github-runner/nixos-runner 0750 github-runner github-runner -"
-      "d /var/log/github-runner 0750 github-runner github-runner -"
-      "d /var/log/github-runner/nixos-runner 0750 github-runner github-runner -"
-    ];
-
     services.github-runners = {
       nixos-runner = {
         enable = true;
         replace = true;
-        user = "github-runner";
-        group = "github-runner";
         tokenFile = "/run/secrets/github-runner/nixos.token";
         url = "https://github.com/Waddenn/nixos-config";
         extraLabels = ["nixos" "self-hosted"];
-        workDir = "/var/lib/github-runner/nixos-runner";
-        serviceOverrides = {
-          StateDirectory = "github-runner/nixos-runner";
-          LogsDirectory = "github-runner/nixos-runner";
-        };
+        workDir = "//home/nixos/github-runner";
       };
     };
   };
