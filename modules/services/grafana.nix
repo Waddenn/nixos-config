7923870d4a722d15bbcfa9e6b4589d4ebe7399af
Waddenn <tpatelas@proton.me>
2025-04-28 19:24:29 +0200

    Implement code changes to enhance functionality and improve performance

--- modules/services/grafana.nix
+++ modules/services/grafana.nix
@@ -10,9 +10,37 @@
   config = lib.mkIf config.grafana.enable {
     services.grafana = {
       enable = true;
+
       settings = {
         server.http_addr = "0.0.0.0";
         server.http_port = 3000;
+      };
+
+      provision = {
+        enable = true;
+
+        datasources.settings = {
+          apiVersion = 1;
+          datasources = [
+            {
+              name = "Prometheus";
+              type = "prometheus";
+              access = "proxy";
+              url = "http://192.168.1.117:9090";
+              isDefault = true;
+            }
+          ];
+        };
+
+        dashboards.settings = {
+          apiVersion = 1;
+          providers = [
+            {
+              name = "node-exporter-dashboards";
+              options.path = "/home/nixos/nixos-config/modules/services/grafana";
+            }
+          ];
+        };
       };
     };
 
