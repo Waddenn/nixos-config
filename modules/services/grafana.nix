18c76a3e8b9e84dd372209dba77afaddec67d9cc
Waddenn <tpatelas@proton.me>
2025-04-28 19:30:46 +0200

    refactor: remove unused Grafana provisioning settings and related configurations

--- modules/services/grafana.nix
+++ modules/services/grafana.nix
@@ -10,42 +10,11 @@
   config = lib.mkIf config.grafana.enable {
     services.grafana = {
       enable = true;
-
       settings = {
         server.http_addr = "0.0.0.0";
         server.http_port = 3000;
       };
-
-      provision = {
-        enable = true;
-
-        datasources.settings = {
-          apiVersion = 1;
-          datasources = [
-            {
-              name = "Prometheus";
-              type = "prometheus";
-              access = "proxy";
-              url = "http://localhost:9090";
-              isDefault = true;
-            }
-          ];
-        };
-
-        dashboards.settings = {
-          apiVersion = 1;
-          providers = [
-            {
-              name = "node-exporter-dashboards";
-              options.path = "/etc/grafana/dashboards";
-            }
-          ];
-        };
-      };
     };
-
-    environment.etc."grafana/dashboards/Node Exporter Full.json".source =
-      /home/nixos/nixos-config/modules/services/grafana/Node-Exporter-Full.json;
 
     networking.firewall.allowedTCPPorts = [3000];
   };
