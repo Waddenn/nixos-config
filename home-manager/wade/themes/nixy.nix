31c1660131a46656767afd573be757b0052d4211
Waddenn <tpatelas@proton.me>
2025-05-06 09:24:06 +0200

    feat: update theme configuration for stylix with new options and cursor settings

--- home-manager/wade/themes/nixy.nix
+++ home-manager/wade/themes/nixy.nix
@@ -1,112 +1,90 @@
-# Wofi is a launcher for Wayland, inspired by rofi.
 {
-  config,
-  pkgs,
   lib,
+  pkgs,
+  config,
   ...
-}: let
-  accent = "#${config.lib.stylix.colors.base0D}";
-  background = "#${config.lib.stylix.colors.base00}";
-  background-alt = "#${config.lib.stylix.colors.base01}";
-  foreground = "#${config.lib.stylix.colors.base05}";
-  font = config.stylix.fonts.serif.name;
-  rounding = config.theme.rounding;
-  font-size = config.stylix.fonts.sizes.popups;
-in {
-  home.packages = with pkgs; [wofi-emoji];
+}: {
+  options.theme = lib.mkOption {
+    type = lib.types.attrs;
+    default = {
+      rounding = 20;
+      gaps-in = 10;
+      gaps-out = 10 * 2;
+      active-opacity = 0.96;
+      inactive-opacity = 0.92;
+      blur = true;
+      border-size = 3;
+      animation-speed = "fast"; # "fast" | "medium" | "slow"
+      fetch = "none"; # "nerdfetch" | "neofetch" | "pfetch" | "none"
+      textColorOnWallpaper =
+        config.lib.stylix.colors.base01; # Color of the text displayed on the wallpaper (Lockscreen, display manager, ...)
 
-  programs.wofi = {
+      bar = {
+        # Hyprpanel
+        position = "top"; # "top" | "bottom"
+        transparent = true;
+        transparentButtons = false;
+        floating = true;
+      };
+    };
+    description = "Theme configuration options";
+  };
+
+  config.stylix = {
     enable = true;
 
-    settings = {
-      allow_markup = true;
-      width = 650;
-      show = "drun";
-      prompt = "Apps";
-      normal_window = true;
-      layer = "top";
-      height = "325px";
-      orientation = "vertical";
-      halign = "fill";
-      line_wrap = "off";
-      dynamic_lines = false;
-      allow_images = true;
-      image_size = 24;
-      exec_search = false;
-      hide_search = false;
-      parse_search = false;
-      insensitive = true;
-      hide_scroll = true;
-      no_actions = true;
-      sort_order = "default";
-      gtk_dark = true;
-      filter_rate = 100;
-      key_expand = "Tab";
-      key_exit = "Escape";
+    # See https://tinted-theming.github.io/tinted-gallery/ for more schemes
+    base16Scheme = {
+      base00 = "09090B"; # Default Background
+      base01 = "1c1e1f"; # Lighter Background (Used for status bars, line number and folding marks)
+      base02 = "313244"; # Selection Background
+      base03 = "45475a"; # Comments, Invisibles, Line Highlighting
+      base04 = "585b70"; # Dark Foreground (Used for status bars)
+      base05 = "cdd6f4"; # Default Foreground, Caret, Delimiters, Operators
+      base06 = "f5e0dc"; # Light Foreground (Not often used)
+      base07 = "b4befe"; # Light Background (Not often used)
+      base08 = "f38ba8"; # Variables, XML Tags, Markup Link Text, Markup Lists, Diff Deleted
+      base09 = "fab387"; # Integers, Boolean, Constants, XML Attributes, Markup Link Url
+      base0A = "f9e2af"; # Classes, Markup Bold, Search Text Background
+      base0B = "a6e3a1"; # Strings, Inherited Class, Markup Code, Diff Inserted
+      base0C = "94e2d5"; # Support, Regular Expressions, Escape Characters, Markup Quotes
+      base0D = "c5afd4"; # Functions, Methods, Attribute IDs, Headings, Accent color
+      base0E = "cba6f7"; # Keywords, Storage, Selector, Markup Italic, Diff Changed
+      base0F = "f2cdcd"; # Deprecated, Opening/Closing Embedded Language Tags, e.g. <?php ?>
     };
 
-    style =
-      lib.mkForce
-      # css
-      ''
-        * {
-          font-family: "${font}";
-          font-weight: 600;
-          font-size: ${toString font-size}px;
-        }
+    cursor = {
+      name = "phinger-cursors-light";
+      package = pkgs.phinger-cursors;
+      size = 20;
+    };
 
-        #window {
-          background-color: ${background};
-          color: ${foreground};
-          border-radius: ${toString rounding}px;
-        }
+    fonts = {
+      monospace = {
+        package = pkgs.nerd-fonts.jetbrains-mono;
+        name = "JetBrains Mono Nerd Font";
+      };
+      sansSerif = {
+        package = pkgs.source-sans-pro;
+        name = "Source Sans Pro";
+      };
+      serif = config.stylix.fonts.sansSerif;
+      emoji = {
+        package = pkgs.noto-fonts-emoji;
+        name = "Noto Color Emoji";
+      };
+      sizes = {
+        applications = 13;
+        desktop = 13;
+        popups = 13;
+        terminal = 13;
+      };
+    };
 
-        #outer-box {
-          padding: 20px;
-        }
-
-        #input {
-          background-color: ${background-alt};
-          border: 0px solid ${accent};
-          color: ${foreground};
-          padding: 8px 12px;
-        }
-
-        #scroll {
-          margin-top: 20px;
-        }
-
-        #inner-box {}
-
-        #img {
-          padding-right: 8px;
-        }
-
-        #text {
-          color: ${foreground};
-        }
-
-        #text:selected {
-          color: ${foreground};
-        }
-
-        #entry {
-          padding: 6px;
-        }
-
-        #entry:selected {
-          background-color: ${accent};
-          color: ${foreground};
-        }
-
-        #unselected {}
-
-        #selected {}
-
-        #input,
-        #entry:selected {
-          border-radius: ${toString rounding}px;
-        }
-      '';
+    polarity = "dark";
+    image = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/anotherhadi/awesome-wallpapers/refs/heads/main/app/static/wallpapers/black-grey-and-white.png";
+      sha256 = "sha256-NQo6sEt7qt82sI8yCxNwSs0+NnWx17oko7MHSap34DI=";
+    };
   };
 }
