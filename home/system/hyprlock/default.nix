a88ce1f6d5827bb71563a82b85dd3c3b6e209fa7
Waddenn <tpatelas@proton.me>
2025-05-17 14:32:15 +0200

    fix: refine Hyprlock configuration by adjusting label positions, font sizes, and input field properties

--- home/system/hyprlock/default.nix
+++ home/system/hyprlock/default.nix
@@ -1,4 +1,3 @@
-# Hyprlock is a lockscreen for Hyprland
 {
   config,
   lib,
@@ -16,7 +15,6 @@ in {
         disable_loading_bar = false;
       };
 
-      # BACKGROUND
       background = {
         monitor = "";
         blur_passes = 0;
@@ -28,60 +26,57 @@ in {
 
       label = [
         {
-          # Day-Month-Date
+          # Date
           monitor = "";
           text = ''cmd[update:1000] echo -e "$(date +"%A, %B %d")"'';
           color = foreground;
-          font_size = 28;
+          font_size = 40;
           font_family = font + " Bold";
-          position = "0, 490";
+          position = "0, 540";
           halign = "center";
           valign = "center";
         }
-        # Time
         {
+          # Time
           monitor = "";
-          text = ''cmd[update:1000] echo "<span>$(date +"%I:%M")</span>"'';
+          text = ''cmd[update:1000] echo "<span>$(date +"%H:%M")</span>"'';
           color = foreground;
-          font_size = 160;
+          font_size = 220;
           font_family = "steelfish outline regular";
           position = "0, 370";
           halign = "center";
           valign = "center";
         }
-        # USER
         {
+          # Username
           monitor = "";
           text = "    $USER";
           color = foreground;
-          outline_thickness = 2;
-          dots_size = 0.2; # Scale of input-field height, 0.2 - 0.8
-          dots_spacing = 0.2; # Scale of dots' absolute size, 0.0 - 1.0
-          dots_center = true;
-          font_size = 18;
+          outline_thickness = 3;
+          font_size = 42;
           font_family = font + " Bold";
-          position = "0, -180";
+          position = "0, -100";
           halign = "center";
           valign = "center";
         }
       ];
 
-      # INPUT FIELD
       input-field = lib.mkForce {
         monitor = "";
-        size = "300, 60";
-        outline_thickness = 2;
-        dots_size = 0.2; # Scale of input-field height, 0.2 - 0.8
-        dots_spacing = 0.2; # Scale of dots' absolute size, 0.0 - 1.0
+        size = "500, 100";
+        outline_thickness = 3;
+        dots_size = 0.2;
+        dots_spacing = 0.2;
         dots_center = true;
-        outer_color = "rgba(25, 25, 25, 0)";
-        inner_color = "rgba(25, 25, 25, 0.1)";
+        outer_color = "rgba(25, 25, 25, 0.2)";
+        inner_color = "rgba(25, 25, 25, 0.25)";
         font_color = foreground;
         fade_on_empty = false;
+        font_size = 38;
         font_family = font + " Bold";
         placeholder_text = "<i>🔒 Enter Password</i>";
         hide_input = false;
-        position = "0, -250";
+        position = "0, -220";
         halign = "center";
         valign = "center";
       };
