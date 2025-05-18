c1f233bf5f0e036227b48f5c165b0f66b94c71a6
Waddenn <tpatelas@proton.me>
2025-05-18 09:55:14 +0200

    fix: restructure fish configuration to properly define program settings and shell initialization

--- modules/programs/fish.nix
+++ modules/programs/fish.nix
@@ -7,6 +7,12 @@
   options.fish.enable = lib.mkEnableOption "Enable fish shell";
 
   config = lib.mkIf config.fish.enable {
-    programs.fish.enable = true;
+    programs.fish = {
+      enable = true;
+
+      shellInit = ''
+        set -gx TERM xterm-256color
+      '';
+    };
   };
 }
