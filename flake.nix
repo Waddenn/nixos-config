f035d31491d20d1091f7230a17be17ed105bb127
Waddenn <tpatelas@proton.me>
2025-05-25 18:13:10 +0200

    Add Glance dashboard service configuration

--- flake.nix
+++ flake.nix
@@ -254,6 +254,9 @@
       jellyseerr = mkServer "jellyseerr" "nixos" [
         {jellyseerr.enable = true;}
       ];
+      glance = mkServer "glance" "nixos" [
+        {glance.enable = true;}
+      ];
     };
 
     checks = lib.genAttrs ["beszel"] (
