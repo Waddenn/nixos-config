b8bdb022d599cd288376d2e080c9f71d0ed9a946
Waddenn <tpatelas@proton.me>
2025-10-16 11:00:26 +0200

    Add GitLab secret files for ActiveRecord and update firewall ports

--- modules/services/gitlab.nix
+++ modules/services/gitlab.nix
@@ -57,6 +57,9 @@
         secretFile = "/var/keys/gitlab/secret";
         otpFile = "/var/keys/gitlab/otp";
         jwsFile = "/var/keys/gitlab/jws";
+        activeRecordPrimaryKeyFile        = "/var/keys/gitlab/ar_primary_key";
+        activeRecordDeterministicKeyFile  = "/var/keys/gitlab/ar_deterministic_key";
+        activeRecordSaltFile              = "/var/keys/gitlab/ar_salt";
       };
 
       extraConfig = {
@@ -69,6 +72,6 @@
       };
     };
 
-    networking.firewall.allowedTCPPorts = [443];
+    networking.firewall.allowedTCPPorts = [443 80];
   };
 }
