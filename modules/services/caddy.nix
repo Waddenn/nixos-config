d9af1e2c583d064a90e75441a79877562cb49010
Waddenn <tpatelas@proton.me>
2025-11-26 12:26:49 +0100

    Update Caddy package hash for cloudflare DNS plugin

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -29,7 +29,7 @@ in {
       enable = true;
       package = pkgs.caddy.withPlugins {
         plugins = ["github.com/caddy-dns/cloudflare@v0.2.1"];
-        hash = "sha256-j+xUy8OAjEo+bdMOkQ1kVqDnEkzKGTBIbMDVL7YDwDY=";
+        hash = "sha256-Dvifm7rRwFfgXfcYvXcPDNlMaoxKd5h4mHEK6kJ+T4A=";
       };
 
       logDir = "/var/log/caddy";
