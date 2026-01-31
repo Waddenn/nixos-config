21195041caba78fd8daf8e7b6b978e9752e1c110
Waddenn <tpatelas@proton.me>
2026-01-31 22:41:53 +0100

    chore: verify reporting and fleet tags

--- flake.nix
+++ flake.nix
@@ -78,3 +78,4 @@
 # trigger build Sat Jan 31 10:34:11 PM CET 2026
 # trigger build 2 Sat Jan 31 10:36:23 PM CET 2026
 # trigger build 3 Sat Jan 31 10:40:01 PM CET 2026
+# trigger build 4 Sat Jan 31 10:41:53 PM CET 2026
