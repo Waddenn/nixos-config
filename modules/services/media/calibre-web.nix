3f5359060089b71dc715f863ffd518e0266b5b69
Waddenn <tpatelas@proton.me>
2026-02-06 22:34:46 +0100

    feat(calibre-web): refactor service configuration to use enable option

--- modules/services/media/calibre-web.nix
+++ modules/services/media/calibre-web.nix
@@ -1,10 +1,18 @@
-{config, ...}: {
-  services.calibre-web = {
-    enable = true;
-    openFirewall = true;
-    options = {
-      enableBookUploading = true;
-      enableBookConversion = true;
+{
+  config,
+  lib,
+  ...
+}: {
+  options.my-services.media."calibre-web".enable = lib.mkEnableOption "Enable Calibre-Web service";
+
+  config = lib.mkIf config.my-services.media."calibre-web".enable {
+    services.calibre-web = {
+      enable = true;
+      openFirewall = true;
+      options = {
+        enableBookUploading = true;
+        enableBookConversion = true;
+      };
     };
   };
 }
