a65fe9181be11676892f3a0795c7dc715896b582
Waddenn <tpatelas@proton.me>
2026-02-07 01:50:02 +0100

    feat(gatus): enable deployment target and canary for pull-updater

--- hosts/gatus/default.nix
+++ hosts/gatus/default.nix
@@ -1,4 +1,6 @@
 {...}: {
   imports = [../../modules/infra/proxmox-lxc.nix];
   my-services.monitoring.gatus.enable = true;
+  my-services.infra.deployment-target.enable = true;
+  my-services.infra.pull-updater.canary = true;
 }
