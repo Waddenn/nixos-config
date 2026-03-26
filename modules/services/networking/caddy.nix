2f453e75eaae79f61776d70eb221eb0e20840676
Waddenn <tpatelas@proton.me>
2026-03-26 16:04:04 +0100

    chore: update Caddy plugin hash and add nixEnvSelector to VSCode settings

--- modules/services/networking/caddy.nix
+++ modules/services/networking/caddy.nix
@@ -63,7 +63,7 @@ in {
       enable = true;
       package = pkgs.caddy.withPlugins {
         plugins = ["github.com/caddy-dns/cloudflare@v0.2.1"];
-        hash = "sha256-Zls+5kWd/JSQsmZC4SRQ/WS+pUcRolNaaI7UQoPzJA0=";
+        hash = "sha256-B5xXld1+IRUAQHm8zkHFqvRp8cqnervVL6XEos5VNkc=";
       };
 
       logDir = "/var/log/caddy";
